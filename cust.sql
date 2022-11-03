ec2748fc7d9ed0a46d23a8b5bcafb8f883c631e9
Sukirtha Lakshmanan <80877212+Sukirthalakshmanan1@users.noreply.github.com>
2022-11-03 18:10:57 +0530

    Add files via upload

---
+++ cust.sql
@@ -0,0 +1,9 @@
+create database customer
+use customer
+create table UserInfo(UserId int primary key,Email varchar(20),passwords varchar(20))
+insert into UserInfo values(1,'suki123@gmail.com','abcd123')
+select * from UserInfo
+
+create table custinfo(logid int primary key,customer_email varchar(20),customer_name varchar(20),log_status varchar(20),descriptions varchar(20))
+insert into custinfo values(1,'suki123@gmail.com','suki','done','laptop')
+select *from custinfo
